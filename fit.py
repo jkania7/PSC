7e10745c185b98710ebb1906c12245f34ebc558b
Joseph Kania <jkania7@users.noreply.github.com>
2017-07-20 01:12:29 -0400

    added comments, added period error to fit, reorganized output

--- fit.py
+++ fit.py
@@ -1,3 +1,6 @@
+"""
+Fits and plots sine wave to period vs orbital phase. Created for PSC 2017.
+"""
 import csv, scipy.optimize, glob, os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,18 +10,20 @@ from scipy.optimize import curve_fit
 rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
 rc('text', usetex=True)
 
-def orbit(x, amp, phase, offset):
-    return amp*np.sin(6.283*x+ phase) + offset
+def orbit(x, amp, phase, offset): #sine fucntion to fit to
+    return amp*np.sin(2.0*np.pi*x+ phase) + offset
 
 if __name__ == "__main__":
-    os.chdir('.')
-    for files in glob.glob('*.csv'):
+    if not os.path.exists('./plots/'): #makes dir to put plots and fits
+        os.makedirs('./plots/')
+    print('\033[33mRed Warning: This program will overwite files in ./plots/\033[0m')
+    for files in glob.glob('*.csv'):#opens all *.csv in directory
         file = files.split('.')[0]
         
         rd = csv.reader(open(files,'rU'))
 
         phase=[];period=[]; period_err=[]
-        for row in rd:
+        for row in rd:#CSV file should be orbital phase | period | period error
             phase.append(float(row[0]))
             period.append(float(row[1]))
             period_err.append(float(row[2]))
@@ -27,16 +32,21 @@ if __name__ == "__main__":
         period = np.array(period)
         period_err = np.array(period_err)
         
-        popt, pcov = curve_fit(orbit, phase, period)
+        popt, pcov = curve_fit(orbit, phase, period, sigma=period_err)#fits the sine wave to the points, taking into account errors in period
 
-        xplot = np.linspace(0, 1, num=1000)
+        out = open('./plots/{0}.txt'.format(file),'w')#writes fits and uncertainties out into a .txt file 
+        out.write('A={0}, phi={1}, b={2}\n'.format(popt[0],popt[1],popt[2]))
+        out.write('std_dev A = {0}, std_dev phi = {1}, std_dev b = {2}'.format(np.sqrt(np.diag(pcov))[0], np.sqrt(np.diag(pcov))[1],np.sqrt(np.diag(pcov))[2]))
+        out.close()
+        
+        phase_dense = np.linspace(0, 1, num=1000)
         plt.plot(phase, period,'ro', label='Observed')
-        plt.plot(xplot, orbit(xplot, *popt),label='Fit w/$A={0:.2E},\phi={1:.2f}, b={2:.2f}$'.format(popt[0],popt[1],popt[2]))
+        plt.plot(phase_dense, orbit(phase_dense, *popt),label='Fit w/$A={0:.2E},\phi={1:.2f}, b={2:.2f}$'.format(popt[0],popt[1],popt[2]))
         plt.title(r'{0} Bary. $P$ fitted w/ $A\sin(2\pi \theta+\phi)+b$'.format(file))
         plt.errorbar(phase, period,  yerr=period_err, linestyle='None')
         plt.xlabel(r'Orbital Phase $(\theta)$')
         plt.margins(0.02,0.02)
         plt.ylabel('Pulse Period (ms)')
         plt.legend(loc='best')
-        plt.savefig("{0}.pdf".format(file))
+        plt.savefig("./plots/{0}.pdf".format(file))
         plt.show()
